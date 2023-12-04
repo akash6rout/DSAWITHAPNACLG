93399cdd9f11827436a2517c417b089cf49fae2b
akash6rout <akash06.rout@gmail.com>
2023-12-04 23:30:25 +0530

    plusfirst

--- github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/PlusLibAlpha/RemoveDuplicateInAnArray.java
+++ github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/PlusLibAlpha/RemoveDuplicateInAnArray.java
@@ -1,9 +1,29 @@
 package com.PlusLibAlpha;
 
 public class RemoveDuplicateInAnArray {
+	
+	public static int mergeSortedArray(int[] nums) {
+		int i=0;
+		for(int j=1;j<nums.length;j++) {
+			if(nums[i]<nums[j]) {
+				int temp=nums[i+1];
+				nums[i+1]=nums[j];
+				nums[j]=temp;
+				i++;
+			}
+		}
+		return i+1;
+	}
 
 	public static void main(String[] args) {
-		
-		//System.out.println("this is first program on plus lib");
+		int nums[]=new int[7];
+		nums[0]=0;
+		nums[1]=1;
+		nums[2]=1;
+		nums[3]=1;
+		nums[4]=2;
+		nums[5]=2;
+		nums[6]=3;
+	System.out.println(mergeSortedArray(nums));
 	}
 }

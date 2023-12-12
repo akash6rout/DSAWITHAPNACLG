af767f1acf7c660ce26392915af4157310745ed5
akash6rout <akash06.rout@gmail.com>
2023-12-12 15:50:04 +0530

    t

--- github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/Trai/Main.java
+++ github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/Trai/Main.java
@@ -2,7 +2,48 @@ package com.Trai;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static class Node{
+		Node children[]=new Node[26];
+		boolean eow=false;
 		
+		Node(){
+			for(int i=0;i<26;i++) {
+				children[i]=null;
+			}
+		}
+	}
+	public static Node root= new Node();
+	
+	public static void insert(String word) {
+		Node curr=root;
+		for(int level=0;level<word.length();level++) {
+			int idx=word.charAt(level)- 'a';
+			if(curr.children[idx]==null) {
+				curr.children[idx]= new Node();
+			}
+			curr=curr.children[idx];
+		}
+		curr.eow=true;
+	}
+	public static boolean search(String key) {
+		Node curr=root;
+		for(int level=0;level<key.length();level++) {
+			int idx=key.charAt(level)-'a';
+			if(curr.children[idx]==null) {
+				return false;
+			}
+			curr=curr.children[idx];
+		}
+		return curr.eow==true;
+	}
+	
+	public static void main(String[] args) {
+		String word[]= {"the","a","there","their","any","thee"};
+		for(int i=0;i<word.length;i++) {
+			insert(word[i]);
+		}
+		
+		System.out.println(search("thee"));
+		System.out.println(search("thor"));
 	}
 }

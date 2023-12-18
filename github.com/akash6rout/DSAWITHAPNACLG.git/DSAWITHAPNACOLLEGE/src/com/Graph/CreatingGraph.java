8aa34a319eaf527e744922b7296a89e367b19036
akash6rout <akash06.rout@gmail.com>
2023-12-18 23:46:35 +0530

    0

--- github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/Graph/CreatingGraph.java
+++ github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/Graph/CreatingGraph.java
@@ -1,6 +1,21 @@
 package com.Graph;
 
+import com.Trai.Main.Node;
+
 public class CreatingGraph {
+	
+	static Node merge(Node l1,Node l2) {
+		if(l1==null)return l2;
+		if(l2==null)return l1;
+		
+		Node ans=l1;
+		
+		while(l1!=null && l2!=null) {
+			Node tempNode=l1;
+			//
+		}
+		return ans;
+	}
 
 	public static void main(String[] args) {
 		

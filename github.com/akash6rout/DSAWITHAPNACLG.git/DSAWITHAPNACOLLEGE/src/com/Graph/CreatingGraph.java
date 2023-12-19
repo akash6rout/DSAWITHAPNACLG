f2eb78a52639eb9fb223e2c7b1962cbc6a3dd1e8
akash6rout <akash06.rout@gmail.com>
2023-12-19 22:07:10 +0530

    creating graph

--- github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/Graph/CreatingGraph.java
+++ github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/Graph/CreatingGraph.java
@@ -1,23 +1,53 @@
 package com.Graph;
 
-import com.Trai.Main.Node;
+import java.util.ArrayList;
 
 public class CreatingGraph {
-	
-	static Node merge(Node l1,Node l2) {
-		if(l1==null)return l2;
-		if(l2==null)return l1;
+	static class Edge{
+		int src;
+		int dest;
+		int wt;
 		
-		Node ans=l1;
-		
-		while(l1!=null && l2!=null) {
-			Node tempNode=l1;
-			//
+		public Edge(int s,int d,int w) {
+			this.src=s;
+			this.dest=d;
+			this.wt=w;
 		}
-		return ans;
 	}
 
 	public static void main(String[] args) {
 		
+		int V=5;
+		ArrayList<Edge> graph[]=new ArrayList[V];
+		for(int i=0;i<V;i++) {
+			graph[i]=new ArrayList<>();
+		}
+		//0 -vertix
+		graph[0].add(new Edge(0, 1, 5));
+		
+		//1 vertix
+		graph[1].add(new Edge(1, 0, 5));
+		
+		graph[1].add(new Edge(1, 0, 5));
+		graph[1].add(new Edge(1, 2, 1));
+		graph[1].add(new Edge(1, 3, 3));
+		
+		//2 vertix
+		graph[2].add(new Edge(2, 1, 1));
+		graph[2].add(new Edge(2, 3, 1));
+		graph[2].add(new Edge(2, 4, 4));
+		
+		//3 vertix
+		graph[3].add(new Edge(3, 1, 3));
+		graph[3].add(new Edge(3, 2, 1));
+		
+		//4 vertix
+		graph[4].add(new Edge(4, 2, 2));
+		
+		//2's neighbors
+		for(int i=0;i<graph[2].size();i++) {
+			Edge edge=graph[2].get(i);
+			System.out.println(edge.dest);
+		}
 	}
 }

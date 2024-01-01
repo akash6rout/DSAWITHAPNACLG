1cdb1ded9b676f7b0908b12c4d53da973bb4c72b
akash6rout <akash06.rout@gmail.com>
2024-01-01 20:46:54 +0530

    dp

--- github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/DP/Main.java
+++ github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/DP/Main.java
@@ -1,9 +1,19 @@
 package com.DP;
 
 public class Main {
+	public static int countWays(int n) {
+		if(n==0) {
+			return 1;
+		}
+		if(n<0) {
+			return 0;
+		}
+	return	countWays(n-1)+countWays(n-2);
+	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("DP");
+		int n=5;//n=3 -> 3ways & n=4 -> 5ways => 8
+		System.out.println(countWays(n));
 	}
 }

8db607bc3934a970fb7daaa52599b18f139d4748
akash6rout <akash06.rout@gmail.com>
2024-01-05 21:20:06 +0530

    q1

--- github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/DP/Question1.java
+++ github.com/akash6rout/DSAWITHAPNACLG.git/DSAWITHAPNACOLLEGE/src/com/DP/Question1.java
@@ -1,8 +1,25 @@
 package com.DP;
 
 public class Question1 {
+	public static int knapsack(int val[],int wt[],int w,int n) {
+		if(w==0||n==0) {
+			return 0;
+		}
+		if(wt[n-1]<=w) {//valid
+			//include
+			int ans1=val[n-1]+knapsack(val, wt, w-wt[n-1], n-1);
+			//exclude
+			int ans2=knapsack(val, wt, w, n-1);
+			return Math.max(ans1, ans2);
+		}else {//not valid
+			return knapsack(val, wt, w, n-1);
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Question one");
+		int val[]= {15,14,10,45,30};
+		int wt[]= {2,5,1,3,4};
+		int w=7;
+		System.out.println(knapsack(val, wt, w, val.length));
 	}
 }
